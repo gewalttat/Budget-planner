b0bc7896df7878e489070a4f359e10a473fbdb96
gewalttat <rbhorses@mail.ru>
2019-07-16 22:52:19 +0300

    load

---
+++ script.js
@@ -0,0 +1,16 @@
+var first = 1;
+let second = 2;
+const third = 3;
+/*var pidor = "ty pidor";
+console.log(pidor);
+let arr = ["blya budu", "second", "third"];
+console.log(arr[0]);
+alert(pidor);
+//var check = confirm("are u here?");
+//console.log(check);
+var check = prompt("are u here?", "yes");
+console.log(check); */
+let incr = 10, decr = 10;
+console.log(++incr, --decr);
+console.log(incr--, decr++);
+console.log(incr, decr);
