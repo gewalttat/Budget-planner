e954ef6308fdac4b2c5577cf80d103250e68c3e8
gewalttat <rbhorses@mail.ru>
2019-07-17 19:16:08 +0300

    the framework of the future application is written. added script interaction with the user through the variables money and time, provides answers to questions to the user through the use of promt. the interaction is displayed in alert.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-var first = 1;
-let second = 2;
-const third = 3;
-/*var pidor = "ty pidor";
-console.log(pidor);
-let arr = ["blya budu", "second", "third"];
-console.log(arr[0]);
-alert(pidor);
-//var check = confirm("are u here?");
-//console.log(check);
-var check = prompt("are u here?", "yes");
-console.log(check); */
-let incr = 10, decr = 10;
-console.log(++incr, --decr);
-console.log(incr--, decr++);
-console.log(incr, decr);
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+budget: money,
+timeData: time,
+expenses: {},
+optionalExpenses: {},
+income: [],
+savings: false,
+};
+let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    secondQuestion = prompt("Во сколько обойдется?", "");
+appData.expenses.frstQuestion = firstQuestion;
+appData.expenses.scndQuestion = secondQuestion;
+alert(appData.budget / 30);
