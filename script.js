abd4f48399b3189640c60d2ba26398eb18741ed3
gewalttat <rbhorses@mail.ru>
2019-07-17 23:55:29 +0300

    added a loop to improve the application's interaction with the user, added conditions for displaying the user's income level

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
+
 let appData = {
 budget: money,
 timeData: time,
@@ -8,8 +9,27 @@ optionalExpenses: {},
 income: [],
 savings: false,
 };
-let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    secondQuestion = prompt("Во сколько обойдется?", "");
-appData.expenses.frstQuestion = firstQuestion;
-appData.expenses.scndQuestion = secondQuestion;
-alert(appData.budget / 30);
+
+for (var i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    b = prompt("Во сколько обойдется?", "");
+    if(typeof(a)==='string' && (typeof(a) != null && 
+    (typeof(b)!=null) && a != '' && b != '')) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {}
+}
+
+appData.moneyPerDay = appData.budget/30;
+
+alert("daily budget: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("минимальный уровень дохода");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("средний уровень дохода");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("высокий уровень дохода");
+}else {
+    throw("произошла ошибка в вводе данных");
+}
